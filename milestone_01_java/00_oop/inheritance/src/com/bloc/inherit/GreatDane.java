fc33e3795729f2663da4f4e67cabff17aef68c50
Shawn Bateman <shawn.bateman@gmail.com>
2014-07-02 12:53:21 -0500

    Completed Inheritance Lesson

--- milestone_01_java/00_oop/inheritance/src/com/bloc/inherit/GreatDane.java
+++ milestone_01_java/00_oop/inheritance/src/com/bloc/inherit/GreatDane.java
@@ -1,3 +1,49 @@
 package com.bloc.inherit;
 
 // CLASS DEFINITION GOES HERE
+class GreatDane extends Dog {
+	@Override
+	void changeSize(boolean grow) {
+		int sizeIndex = getSizeIndex();
+		sizeIndex = sizeIndex + (grow ? 1 : -1);
+		if (sizeIndex > 4) {
+			sizeIndex = 4;
+		} else if (sizeIndex < 0) {
+			sizeIndex = 0;
+		}
+		setSize(fromSizeIndex(sizeIndex));
+	}
+		@Override
+		int getSizeIndex(String size) {
+		if (size == null) {
+			// Return default "average" when missing size
+			return 2;
+		}
+		switch(size) {
+			case "tiny": return 0;
+			case "small": return 1;
+			case "average": return 2;
+			case "large": return 3;
+			case "huge": return 4;
+			default: return 2;
+		}
+	}
+
+	/*
+	 * fromSizeIndex
+	 * @param index the index into the sizes array
+	 * @return a String, one of {"tiny", "small", "average", "large"}
+	 */
+	@Override
+	String fromSizeIndex(int index) {
+		switch(index) {
+			case 0: return "tiny";
+			case 1: return "small";
+			case 2: return "average";
+			case 3: return "large";
+			case 4:
+			default: return "huge";
+		}
+	}
+
+}
