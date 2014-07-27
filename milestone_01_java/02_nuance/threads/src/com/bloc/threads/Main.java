4c24c6573cad4988f8b149ae305de17f19fd34e9
Shawn Bateman <shawn.bateman@gmail.com>
2014-07-26 23:00:09 -0500

    Added Scanner input and learned about the difference between .equals and ==

--- milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
+++ milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
@@ -1,14 +1,37 @@
 package com.bloc.threads;
+import java.util.Scanner;
 
 
 
 public class Main extends Object {
+	private static Boolean viewImage() {
+		Boolean answer = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Would you like to view the image y/n?");
+		String view = sc.next();
+		System.out.println(view.getClass().getName());
+		if(view.equals("y")){
+				answer = true;
+			} else if(view.equals("n")) {
+				answer = false;
+			} else {
+				System.out.println("Please choose 'y' or 'n'");
+				viewImage();
+			}
+		return answer;
+		}
 
 	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the URL for your image:");
+		String url = sc.next();
+		Boolean launch = viewImage();
+		
+
 		
 
 		// Extract content beneath to ImageGetter
-		new ImageGetter("http://www.fordesigner.com/imguploads/Image/cjbc/zcool/png20080526/1211782272.png", false).start();
+		new ImageGetter(url, launch).start();
 
 
 		// ^^^ Extract the above to ImageGetter
